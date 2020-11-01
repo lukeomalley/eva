a32f16d3c56139cbbaca5f8a449ed532ba0f9020
Luke O'Malley <Lukeomalley95@gmail.com>
2020-11-01 10:53:39 -0500

    added test runner util to clean up test suites

--- tests/eva.test.js
+++ tests/eva.test.js
@@ -1,7 +1,4 @@
-/* eslint-disable no-console */
-const assert = require('assert');
-const Environment = require('../src/Environment');
-const Eva = require('../src/Eva');
+const testEva = require('./test-runner');
 
 function testEval() {
   const tests = [
@@ -121,44 +118,7 @@ function testEval() {
     },
   ];
 
-  // ===========================================================================
-  // Test Runner
-  // ===========================================================================
-
-  let testHasFailed = false;
-  tests.forEach((test) => {
-    const eva = new Eva(
-      new Environment({
-        null: null,
-        true: true,
-        false: false,
-      }),
-    );
-
-    // Execute the code
-    let result;
-    try {
-      result = eva.eval(test.input);
-    } catch (err) {
-      console.log(
-        `[test - ${test.name}] RuntimeError: ${err.message}`,
-      );
-    }
-
-    // Compare the results
-    try {
-      assert.strictEqual(result, test.expected);
-    } catch (err) {
-      testHasFailed = true;
-      console.log(
-        `[test - ${test.name}] Failed: expected ${test.expected}, but got ${result}`,
-      );
-    }
-  });
-
-  if (!testHasFailed) {
-    console.log(`Ok: ${tests.length} tests passed! 🎉`);
-  }
+  testEva(tests);
 }
 
 testEval();
