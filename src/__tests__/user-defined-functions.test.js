567cfe13506f7ad6c2ebab369d0b58a93ce7032e
Luke O'Malley <Lukeomalley95@gmail.com>
2020-11-01 13:14:56 -0500

    implemented tests for user defined functions

--- src/__tests__/user-defined-functions.test.js
+++ src/__tests__/user-defined-functions.test.js
@@ -6,7 +6,7 @@ function TestUserDefinedFunctions() {
 
   const tests = [
     {
-      name: 'Functons 2',
+      name: 'Functons 1',
       input: `
         (begin
           (def square (x)
@@ -16,6 +16,51 @@ function TestUserDefinedFunctions() {
         )
       `,
       expected: 25,
+    },
+
+    {
+      name: 'Functons 2',
+      input: `
+        (begin
+          (var z 100)
+
+          (def calc (x y)
+            (begin
+              (var z 1000)
+              (+ z (+ x y))
+            )
+          )
+
+          (calc z 10)
+        )
+      `,
+      expected: 1110,
+    },
+
+    {
+      name: 'Functons 3',
+      input: `
+        (begin
+          (var value 100)
+
+          (def calc (x y)
+            (begin
+              (var z (+ x y))
+
+              (def inner (foo)
+                (+ (+ foo z) value)
+              )
+
+              inner
+            )
+          )
+
+          (var fn (calc 10 20))
+
+          (fn 30)
+        )
+      `,
+      expected: 160,
     },
   ];
 
